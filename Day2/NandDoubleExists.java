cd20641b08045b74a1dea0c67c239cdee8e3a766
SpandanDuari <spandanduari@gmail.com>
2024-12-06 08:18:29 +0530

    day2

---
+++ Day2/NandDoubleExists.java
@@ -0,0 +1,15 @@
+package Day2;
+
+class Solution {
+    public boolean checkIfExist(int[] arr) {
+        int len = arr.length;
+        for (int i = 0; i < len; i++) {
+            for (int j = 0; j < len; j++) {
+                if (i != j && arr[i] == 2 * arr[j]) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
